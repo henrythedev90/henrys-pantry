6dad881cc9699adf0f90c399a15540fbbef836eb
henrythedev90 <henrythedev90@gmail.com>
2025-01-24 17:58:22 -0500

    cannot worry about middleware rn

--- pages/api/users/protected.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { authenticateMiddleWare } from "../../../middlewares/authMiddleware";
-
-async function protectedHandler(req: NextApiRequest, res: NextApiResponse) {
-  const user = (req as any).user;
-  res.status(200).json({ message: "Access granted!", user });
-}
-
-export default authenticateMiddleWare(protectedHandler);
