ca0233d6c762e0c8bf5abf151f075708fbb79f9c
Henry <henrythedev90@gmail.com>
2025-01-30 20:08:45 -0500

    updated from domains to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["img.spoonacular.com", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
   async redirects() {
     return [
